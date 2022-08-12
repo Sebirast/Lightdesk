83d38ecfbce62e277d025eefbf0bb453a54691d9
Sebastian Rast <sebastian.rast@outlook.com>
2022-08-12 17:49:46 +0200

    added example project in order to test all the libs testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,107 @@
-#include <Arduino.h>
+#include <menu.h>
+#include <menuIO/adafruitGfxOutRA8875.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_RA8875.h>
+#include <menuIO/chainStream.h>
+#include <menuIO/serialIn.h>
+#include <menuIO/encoderIn.h>
+#include <ClickEncoder.h>
+#include <menuIO/clickEncoderIn.h>
+#include <TimerThree.h>
+#include <Encoder.h>
+
+using namespace Menu;
+
+#define LEDPIN LED_BUILTIN
+#define MAX_DEPTH 1
+
+int timeOn=10;
+int timeOff=90;
+int fokus = 0;
+int fokusFein = 0;
+int dimmer = 0;
+
+
+Adafruit_RA8875 gfx(10, 9);
+
+MENU(mainMenu, "Blink menu ", Menu::doNothing, Menu::noEvent, Menu::noStyle
+  ,FIELD(timeOn,"On","ms",0,1000,10,1, Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,FIELD(fokus,"fokus","ms",0,10000,10,1,Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,FIELD(fokusFein,"fokusFein","ms",0,10000,10,1,Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,FIELD(dimmer,"dimmer","ms",0,10000,10,1,Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,EXIT("<Back")
+);
+
+MENU(subMenu, "Dimmer", Menu::doNothing, Menu::noEvent, Menu::noStyle
+  ,FIELD(timeOn,"On","ms",0,1000,10,1, Menu::doNothing, Menu::noEvent, Menu::noStyle)
+  ,FIELD(fokus,"fokus","ms",0,10000,10,1,Menu::doNothing, Menu::noEvent, Menu::noStyle)
+);
+
+serialIn serial(Serial);
+ClickEncoder clickEncoder(4,5,6);
+ClickEncoderStream encStream(clickEncoder,1);
+MENU_INPUTS(in,&encStream,&serial);
+void timerIsr() {clickEncoder.service();}
+
+// MENU_INPUTS(in, &encStream);
+
+#define RA8875_GRAY RGB565(128, 128, 128)
+
+const colorDef<uint16_t> colors[6] MEMMODE={
+  {{(uint16_t)RA8875_BLACK,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_BLACK, (uint16_t)RA8875_BLUE,  (uint16_t)RA8875_BLUE}},//bgColor
+  {{(uint16_t)RA8875_GRAY, (uint16_t)RA8875_GRAY},  {(uint16_t)RA8875_WHITE, (uint16_t)RA8875_WHITE, (uint16_t)RA8875_WHITE}},//fgColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_YELLOW,(uint16_t)RA8875_YELLOW,(uint16_t)RA8875_RED}},//valColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_WHITE, (uint16_t)RA8875_YELLOW,(uint16_t)RA8875_YELLOW}},//unitColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_GRAY},  {(uint16_t)RA8875_BLACK, (uint16_t)RA8875_BLUE,  (uint16_t)RA8875_WHITE}},//cursorColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_YELLOW},{(uint16_t)RA8875_BLUE,  (uint16_t)RA8875_RED,   (uint16_t)RA8875_RED}},//titleColor
+};
+
+uint16_t textScale = 3;
+
+
+#define GFX_HEIGHT 480
+#define GFX_WIDTH 800
+#define FONTW 21
+#define FONTH 21
+
+MENU_OUTPUTS(out,MAX_DEPTH
+  ,ADAGFX_OUT(gfx,colors,160*textScale,FONTW*textScale,{0,0,GFX_WIDTH / FONTW, GFX_HEIGHT / FONTH})
+  ,NONE
+)
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
+
+Encoder enc(7, 8);
+Encoder enc2(2, 3);
+Encoder enc3(14, 15);
 
 void setup() {
+  gfx.begin(RA8875_800x480);
+  gfx.displayOn(true);
+  gfx.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
+  gfx.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
+  gfx.PWM1out(255);
+
+  gfx.textMode();
+  gfx.fillScreen(RA8875_BLACK);
+  gfx.textEnlarge(3);
+
   Serial.begin(9600);
+  // while(!Serial);
+  Serial.println("Menu 4.x");
+  Serial.println("Use keys + - * /");
+  Serial.println("to control the menu navigation");
+
+  // enc.begin();
+  Timer3.initialize(1000);
+  Timer3.attachInterrupt(timerIsr);
 }
 
+
 void loop() {
-  Serial.println("Hello World");
+  nav.poll();
+  fokus = enc.read();
+  fokusFein = enc2.read();
+  dimmer = enc3.read();
+  nav.doInput();
 }
