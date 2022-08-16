d5d5436034b8af0d92b5ee3fa3ded157a6d644af
Sebastian Rast <sebastian.rast@outlook.com>
2022-08-16 14:52:56 +0200

    put the rest of menu definition into the header file

--- include/paramterMenu.h
+++ include/paramterMenu.h
@@ -1,9 +1,12 @@
 #include <menu.h>
 
+using namespace Menu;
+
+#define MAX_DEPTH 4
+
 int timeOn = 0;
 
 Adafruit_RA8875 gfx(10, 9);
-
 
 int foo = 0;
 // paramter
@@ -67,6 +70,7 @@ result drawBackGround()
   return proceed;
 };
 
+
 MENU(light, "Intensity and colour", drawBackGround, Menu::enterEvent | Menu::exitEvent, Menu::wrapStyle
   ,SUBMENU(shutter)
   ,SUBMENU(dimmer)
@@ -99,3 +103,37 @@ MENU(mainMenu, "Main Menu", Menu::doNothing, Menu::noEvent, Menu::wrapStyle
   ,SUBMENU(parameterMenu)  
   ,SUBMENU(effectMenu)
 );
+
+serialIn serial(Serial);
+ClickEncoder clickEncoder(4,5,6);
+ClickEncoderStream encStream(clickEncoder,1);
+MENU_INPUTS(in,&encStream,&serial);
+void timerIsr() {clickEncoder.service();}
+
+// MENU_INPUTS(in, &encStream);
+
+#define RA8875_GRAY RGB565(128, 128, 128)
+
+const colorDef<uint16_t> colors[6] MEMMODE={
+  {{(uint16_t)RA8875_BLACK,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_BLACK, (uint16_t)RA8875_BLUE,  (uint16_t)RA8875_BLUE}},//bgColor
+  {{(uint16_t)RA8875_GRAY, (uint16_t)RA8875_GRAY},  {(uint16_t)RA8875_WHITE, (uint16_t)RA8875_WHITE, (uint16_t)RA8875_WHITE}},//fgColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_YELLOW,(uint16_t)RA8875_YELLOW,(uint16_t)RA8875_RED}},//valColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_WHITE, (uint16_t)RA8875_YELLOW,(uint16_t)RA8875_YELLOW}},//unitColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_GRAY},  {(uint16_t)RA8875_BLACK, (uint16_t)RA8875_BLUE,  (uint16_t)RA8875_WHITE}},//cursorColor
+  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_YELLOW},{(uint16_t)RA8875_BLUE,  (uint16_t)RA8875_RED,   (uint16_t)RA8875_RED}},//titleColor
+};
+
+uint16_t textScale = 3;
+
+
+#define GFX_HEIGHT 480
+#define GFX_WIDTH 800
+#define FONTW 21
+#define FONTH 21
+
+MENU_OUTPUTS(out,MAX_DEPTH
+  ,ADAGFX_OUT(gfx,colors,160*textScale,FONTW*textScale,{0,0,GFX_WIDTH / FONTW, GFX_HEIGHT / FONTH})
+  ,NONE
+)
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
--- src/main.cpp
+++ src/main.cpp
@@ -11,46 +11,9 @@
 #include <Encoder.h>
 
 // menus
+
+
 #include "paramterMenu.h"
-
-using namespace Menu;
-
-#define MAX_DEPTH 4
-
-
-serialIn serial(Serial);
-ClickEncoder clickEncoder(4,5,6);
-ClickEncoderStream encStream(clickEncoder,1);
-MENU_INPUTS(in,&encStream,&serial);
-void timerIsr() {clickEncoder.service();}
-
-// MENU_INPUTS(in, &encStream);
-
-#define RA8875_GRAY RGB565(128, 128, 128)
-
-const colorDef<uint16_t> colors[6] MEMMODE={
-  {{(uint16_t)RA8875_BLACK,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_BLACK, (uint16_t)RA8875_BLUE,  (uint16_t)RA8875_BLUE}},//bgColor
-  {{(uint16_t)RA8875_GRAY, (uint16_t)RA8875_GRAY},  {(uint16_t)RA8875_WHITE, (uint16_t)RA8875_WHITE, (uint16_t)RA8875_WHITE}},//fgColor
-  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_YELLOW,(uint16_t)RA8875_YELLOW,(uint16_t)RA8875_RED}},//valColor
-  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_BLACK}, {(uint16_t)RA8875_WHITE, (uint16_t)RA8875_YELLOW,(uint16_t)RA8875_YELLOW}},//unitColor
-  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_GRAY},  {(uint16_t)RA8875_BLACK, (uint16_t)RA8875_BLUE,  (uint16_t)RA8875_WHITE}},//cursorColor
-  {{(uint16_t)RA8875_WHITE,(uint16_t)RA8875_YELLOW},{(uint16_t)RA8875_BLUE,  (uint16_t)RA8875_RED,   (uint16_t)RA8875_RED}},//titleColor
-};
-
-uint16_t textScale = 3;
-
-
-#define GFX_HEIGHT 480
-#define GFX_WIDTH 800
-#define FONTW 21
-#define FONTH 21
-
-MENU_OUTPUTS(out,MAX_DEPTH
-  ,ADAGFX_OUT(gfx,colors,160*textScale,FONTW*textScale,{0,0,GFX_WIDTH / FONTW, GFX_HEIGHT / FONTH})
-  ,NONE
-)
-
-NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
 
 Encoder enc(7, 8);
 Encoder enc2(2, 3);
@@ -81,5 +44,5 @@ void setup() {
 void loop() {
   nav.poll();
   nav.doInput();
-  Serial.println(dimmer.getText());
+  Serial.print(nav.active().getText());
 }
