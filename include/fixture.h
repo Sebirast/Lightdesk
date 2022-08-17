f0086e10ffb75240d5e3276a574f7f9fd51df49f
Sebastian Rast <sebastian.rast@outlook.com>
2022-08-17 16:31:16 +0200

    fixed bug in fixture.h and implemented the new dmx library

--- include/fixture.h
+++ include/fixture.h
@@ -1,51 +1,56 @@
 #pragma once
 
-#include <DmxSimple.h>
+#include "TeensyDMX.h"
 
 namespace fixture
 {
   class Fixture
   {
-    struct Param
-    {
-      dimmer: int, 
-      dimmerFine: int, 
-      firstColorWheel: int, 
-      colorWheel1: int,
-      colorWheel2: int,
-      secondGoboWheel: int,
-      prisma: int,
-      iris: int,
-      irisDiameter: int,
-      focus: int, // TODO focusfine
-      pan: int, 
-      panFine: int,
-      tilt: int,
-      tiltFine: int,
-      zoom: int,
-      panAndTiltSpeed: int,
-      profilFilter1: int,
-      profilFilter2: int,
-      cyan: int,
-      magenta: int,
-      yellow: int,
-    }
+    public:
+      struct Param
+      {
+        int shutter;
+        int dimmer; 
+        int dimmerFine;
+        int colorWheel1;
+        int colorWheel2;
+        int goboWheel1;
+        int goboWheel2;
+        int prisma;
+        int iris;
+        int irisDiameter;
+        int focus; // TODO focusfine
+        int pan;
+        int panFine;
+        int tilt;
+        int tiltFine;
+        int zoom;
+        int panAndTiltSpeed;
+        int profilFilter1;
+        int profilFilter2;
+        int cyan;
+        int magenta;
+        int yellow;
+      };
 
-    enum FixtureType
-    {
-      MAC550, 
-      MAC600E
-    }
+    
+      enum FixtureType
+      {
+        MAC550, 
+        MAC600E
+      };
+
+      Fixture::Param* channels = NULL;
+
+      Fixture(qindesign::teensydmx::Sender* dmx, Fixture::FixtureType type, uint16_t address);
+      void set(uint16_t channel, uint8_t value);
+      uint8_t get(fixture::Fixture::Param channel);
+      void play();
 
     private:
-      DmxSimple* dmx;
+      qindesign::teensydmx::Sender* dmx;
       uint16_t address;
       Fixture::Param currentValues;
-
-    public:
-      Fixture(DmxSimple* dmx, Fixture::FixtureType type, uint16_t adress)
-      {
-
-      }
-  }
+      Fixture::FixtureType type;
+  };
 }
