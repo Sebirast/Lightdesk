ebb1f39429ee6f6c51b5860d7ad3c380512ecaeb
Sebastian Rast <sebastian.rast@outlook.com>
2022-08-17 16:27:51 +0200

    implemented fixture config

---
+++ include/fixtureConfig.h
@@ -0,0 +1,49 @@
+#pragma once
+
+fixture::Fixture::Param MAC550_config =
+{
+  1, // shutter
+  2, // dimmer 
+  3, // dimmer fine 
+  4, // colorWheel1
+  6, // colorWheel2
+  8, // goboWheel1
+  11, // goboWheel2
+  15, // prisma
+  16, // iris
+  17, // iris diameter
+  18, // focus
+  22, // pan
+  23, // pan fine
+  24, // tilt
+  25, // tilt fine
+  26, // pan / tilt speed
+  0, 
+  0, 
+  0, 
+  0
+};
+
+fixture::Fixture::Param MAC600E_config =
+{
+  1, // shutter
+  2, // dimmer
+  0, 
+  6, // colorWheel1
+  0, 
+  0, 
+  0, 
+  0, 
+  0,
+  9,
+  10,
+  11, 
+  12,
+  0,
+  13,
+  7, 
+  8, 
+  3, 
+  4, 
+  5
+};
